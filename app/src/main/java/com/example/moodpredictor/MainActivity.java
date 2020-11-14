8f016c4dc9bfe1784600f4e587dd4d9652c7bd7b
wak7222 <wak7222@rit.edu>
2020-11-14 18:49:19 -0500

    Setting up Spotify SDK

--- app/src/main/java/com/example/moodpredictor/MainActivity.java
+++ app/src/main/java/com/example/moodpredictor/MainActivity.java
@@ -12,8 +12,20 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+
+    private static final String CLIENT_ID = "97ed99b3b2bc461da6bfe088bbd4d8c9";
+    private static final String REDIRECT_URI = "http://com.example.moodpredictor/callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +55,36 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(context, SplashScreenActivity.class);
         startActivity(intent);
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ConnectionParams connectionParams = new ConnectionParams.Builder(CLIENT_ID)
+                .setRedirectUri(REDIRECT_URI).showAuthView(true).build();
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("William", "Connected!");
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.d("William", "Spotify failed to connect.");
+                    }
+                });
+
+    }
+
+    private void connected() {
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
     }
 }
