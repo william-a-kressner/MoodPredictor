4b6bb9ad9168792164b0e23e5ee11670c3afe5e4
wak7222 <wak7222@rit.edu>
2020-11-17 00:05:33 -0500

    Population to database is successful!!

--- app/src/main/java/com/example/moodpredictor/ListeningData.java
+++ app/src/main/java/com/example/moodpredictor/ListeningData.java
@@ -1,11 +1,23 @@
 package com.example.moodpredictor;
 
+import com.spotify.protocol.types.Track;
+
 import java.util.ArrayList;
 
 public class ListeningData {
-    private ArrayList<String> songs;
+    public String album;
+    public String track;
+    public String artist;
+
 
     public ListeningData(){
-        songs = new ArrayList<>();
+
     }
+
+    public ListeningData(Track track){
+        this.album = track.album.name;
+        this.track = track.name;
+        this.artist = track.artist.name;
+    }
+
 }
