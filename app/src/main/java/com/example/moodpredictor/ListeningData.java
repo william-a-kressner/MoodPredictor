7f58f2f4ff003c0ed412d230500778d28133455c
wak7222 <wak7222@rit.edu>
2020-12-07 21:32:38 -0500

    Database schema has been updated.

--- app/src/main/java/com/example/moodpredictor/ListeningData.java
+++ app/src/main/java/com/example/moodpredictor/ListeningData.java
@@ -2,13 +2,14 @@ package com.example.moodpredictor;
 
 import com.spotify.protocol.types.Track;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 
 public class ListeningData {
     public String album;
     public String track;
     public String artist;
-
+    public TimeOfDay timeOfDay;
+    public LocalDateTime localDateTime;
 
     public ListeningData(){
 
@@ -18,6 +19,16 @@ public class ListeningData {
         this.album = track.album.name;
         this.track = track.name;
         this.artist = track.artist.name;
+        this.localDateTime = LocalDateTime.now();
+        int hour = LocalDateTime.now().getHour();
+        if (4 <= hour && hour <10)
+            this.timeOfDay = TimeOfDay.MORNING;
+        else if (10<=hour && hour<16)
+            this.timeOfDay = TimeOfDay.AFTERNOON;
+        else if (16<=hour && hour < 22)
+            this.timeOfDay = TimeOfDay.EVENING;
+        else
+            this.timeOfDay = TimeOfDay.NIGHT;
     }
 
 }
